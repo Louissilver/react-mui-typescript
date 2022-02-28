c7d96bf851adef155a8553c4bcc7ac116def1bf1
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2022-02-28 16:09:20 -0300

    feat: incluindo opção de fechar e abrir o menu lateral

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,27 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Button } from '@mui/material';
-import { useAppThemecontext } from '../shared/contexts';
+import { useAppThemecontext, useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
   const { toggleTheme } = useAppThemecontext();
+  const { toggleDrawerOpen } = useDrawerContext();
   return (
     <Routes>
       <Route
         path="/pagina-inicial"
         element={
-          <Button variant="contained" color="primary" onClick={toggleTheme}>
-            Toggle theme
-          </Button>
+          <>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={toggleDrawerOpen}
+            >
+              Toggle drawer
+            </Button>
+            <Button variant="contained" color="primary" onClick={toggleTheme}>
+              Toggle theme
+            </Button>
+          </>
         }
       />
       <Route path="*" element={<Navigate to="/pagina-inicial"></Navigate>} />
